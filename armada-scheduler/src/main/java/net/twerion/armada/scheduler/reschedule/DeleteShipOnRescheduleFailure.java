c8ca0152be19926f440ed75fc82c1bfb90ba4594
Merlin <merlinosayimwen@gmail.com>
2019-06-19 14:31:06 +0200

    Add DeletShipOnRescheduleFailure strategy

---
+++ armada-scheduler/src/main/java/net/twerion/armada/scheduler/reschedule/DeleteShipOnRescheduleFailure.java
@@ -0,0 +1,33 @@
+package net.twerion.armada.scheduler.reschedule;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.twerion.armada.Ship;
+import net.twerion.armada.api.DeleteShipRequest;
+import net.twerion.armada.api.ShipServiceGrpc;
+
+public class DeleteShipOnRescheduleFailure implements RescheduleFailureStrategy {
+  private Logger LOG = LogManager.getLogger(DeleteShipOnRescheduleFailure.class);
+  private ShipServiceGrpc.ShipServiceFutureStub shipService;
+
+  @Override
+  public void onFailure(Ship ship) {
+    LOG.warn(
+      "Deleting ship {}, because it could not be rescheduled", ship.getId());
+
+    try {
+      shipService.delete(DeleteShipRequest.newBuilder()
+        .setShipId(ship.getId())
+        .build()
+      );
+      LOG.info("Successfully deleted unscheduled ship {}", ship.getId());
+    } catch (Exception rpcFailure) {
+      String errorMessage = String.format(
+        "Could not delete unscheduled ship %s", ship.getId()
+        );
+      LOG.error(errorMessage, rpcFailure);
+      LOG.error("Ship will no longer be scheduled and may create a leak");
+    }
+  }
+}
